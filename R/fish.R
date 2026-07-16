c5db42ef31c070671cd0b28294ce7dc710c2fa93
Joe Roe <git@joeroe.io>
2026-07-16 04:24:46 +0200

    Fix moved FISH URL

--- R/fish.R
+++ R/fish.R
@@ -1,12 +1,12 @@
 # fish.R
 # Functions for Historic England's FISH controlled vocabularies
-# <https://www.heritage-standards.org.uk/fish-vocabularies/>
+# <https://heritage-standards.org.uk/fish-vocabularies/>
 
 #' Read a FISH controlled vocabulary
 #'
 #' Reads controlled vocabularies from Historic England's FISH (Forum on
 #' Information Standards in Heritage) group. These can be downloaded from:
-#' <https://www.heritage-standards.org.uk/fish-vocabularies/>.
+#' <https://heritage-standards.org.uk/fish-vocabularies/>.
 #'
 #' @param path Path or URL to a vocabulary in FISH's CSV format. Can be
 #'   either a `.zip` archive or a directory containing already uncompressed
@@ -17,7 +17,7 @@
 #'
 #' @references
 #' * Forum on Information Standards in Heritage, "FISH Thesaurus Table Structure",
-#'   Available from: <https://www.heritage-standards.org.uk/fish-vocabularies/>.
+#'   Available from: <https://heritage-standards.org.uk/fish-vocabularies/>.
 #'
 #' @export
 #'
